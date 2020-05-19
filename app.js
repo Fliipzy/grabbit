ab21374ecabf0c63887ecf37bd418a5a887a2cf7
Fliipzy <Frederiklundbeck@live.dk>
2020-05-19 23:42:06 +0200

    Added Knex & Objection

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const app = express()
 //Get port from possible set variable else 3000
 const PORT = process.env.PORT ? process.env.PORT : 3000;
 
+//Setup bodyparsing
+const bodyparser = require('body-parser')
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json)
+
 //Static serving from ./public
 app.use(express.static('public'))
 
 //Setup ejs view engine
 app.set('view engine', 'ejs')
+
+//Setup Objection & Knex
+const { Model } = require('objection')
+const knexfile = require('./database/knexfile.js')
+Model.knex(knexfile)
+
 
 //Setup routes
 const authRouter = require('./routes/auth.js')
@@ -17,7 +28,8 @@ const productRouter = require('./routes/product.js')
 const storeRouter = require('./routes/store.js')
 const userRouter = require('./routes/user.js')
 
-app.use('/', indexRouter)
+app.use(authRouter)
+app.use(indexRouter)
 app.use('/users', userRouter)
 app.use('/stores', storeRouter)
 app.use('/products', productRouter)
